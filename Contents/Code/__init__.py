da806df97f3bb435efa18d994e2e6463e64c5c55
Menno van den Ende <mentosmenno2@hotmail.com>
2018-02-08 19:43:42 +0100

    add programs list

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,3 +1,7 @@
+import string
+import json
+import urllib
+
 NAME   = 'KIJK'
 ICON   = 'icon-default.png'
 ART    = 'art-default.jpg'
@@ -22,8 +26,12 @@ CHANNELS = [
 	}
 ]
 
-import string
-AZ = string.ascii_uppercase
+AZUPPER = string.ascii_uppercase
+AZ_LOWER = string.ascii_lowercase
+DIGS = string.digits
+API_URL_V1 = 'https://api.kijk.nl/v1/'
+API_URL_V2 = 'https://api.kijk.nl/v2/'
+
 RE_SERIES = 'http://kijk.nl/(.*?)/(.*?)'
 
 ####################################################################################################
@@ -43,131 +51,59 @@ def MainMenu():
 
 	oc = ObjectContainer()
 
+	# oc.add(DirectoryObject(
+	# 		title = 'Gemist',
+	# 		thumb = R(ICON),
+	# 		key = Callback(gemist)
+	# ))
+	# oc.add(DirectoryObject(
+	# 		title = 'Meest Bekeken',
+	# 		thumb = R(ICON),
+	# 		key = Callback(meestBekeken)
+	# ))
 	oc.add(DirectoryObject(
-			title = 'Gemist',
+			title = 'Programmalijst',
 			thumb = R(ICON),
-			key = Callback(gemist)
-	))
-	oc.add(DirectoryObject(
-			title = 'Meest Bekeken',
-			thumb = R(ICON),
-			key = Callback(meestBekeken)
-	))
-	oc.add(DirectoryObject(
-			title = 'A-Z',
-			thumb = R(ICON),
-			key = Callback(AtoZ)
+			key = Callback(ProgramsList, title2='Programmalijst')
 	))
 
 	return oc
 
 ####################################################################################################
-@route(PREFIX + '/gemist')
-def gemist():
+@route(PREFIX + '/programsList')
+def ProgramsList(title2=''):
 
-	oc = ObjectContainer(title2='Gemist')
+	oc = ObjectContainer(title2='Programmalijst')
 
-	for channel in CHANNELS:
-		oc.add(DirectoryObject(
-			title = channel['name'],
-			thumb = R(channel['slug']+'.png'),
-			key = Callback(gemistForChannel, channel=channel['slug'])
-		))
-
-	return oc
-
-####################################################################################################
-@route(PREFIX + '/gemistForChannel')
-def gemistForChannel(channel):
-
-	oc = ObjectContainer(title2=(item['name'] for item in CHANNELS if item["slug"] == channel).next())
-
-	html = HTML.ElementFromURL('http://www.kijk.nl/ajax/section/overview/missed_MissedChannel-'+channel)
-
-	days = html.xpath('//h2[@class="showcase-heading"]//text()')
-
-	for i,day in enumerate(days):
-		oc.add(DirectoryObject(
-			title = day.title(),
-			thumb = R(channel+'.png'),
-			key = Callback(gemistForDay, channel=channel, day=i, dayName=day.title())
-		))
-
-	return oc
-
-####################################################################################################
-@route(PREFIX + '/gemistForDay')
-def gemistForDay(channel, day, dayName):
-
-	html = HTTP.Request('http://www.kijk.nl/ajax/section/overview/missed_MissedChannel-'+channel).content.split('<hr>')[int(day)]
-
-	return ListRows(HTML.ElementFromString(html), title2=dayName)
-
-####################################################################################################
-@route(PREFIX + '/meestBekeken')
-def meestBekeken():
-
-	return ListRowsFromAJAX('home_Episodes-popular/1/20', title2='Meest Bekeken')
-
-####################################################################################################
-@route(PREFIX + '/AtoZ')
-def AtoZ():
-
-	oc = ObjectContainer(title2='A-Z')
-
-	for i in range(0, len(AZ)):
-		oc.add(DirectoryObject(
-			title = AZ[i],
-			thumb = R(ICON),
-			key = Callback(ListRowsFromAJAX, path='programs-abc-'+AZ[i], title2=AZ[i])
-		))
-
-	return oc
-
-####################################################################################################
-@route(PREFIX + '/ListRowsFromAJAX')
-def ListRowsFromAJAX(path, FallbackIcon='', title2=''):
-
-	return ListRows(HTML.ElementFromURL('http://www.kijk.nl/ajax/section/overview/'+path), FallbackIcon, title2)
-
-####################################################################################################
-def ListRows(html, FallbackIcon='', title2=''):
-
-	oc = ObjectContainer(title2=title2)
-	elements = html.xpath('//div[a/div/@class="info "]')
+	jsonObj = getFromAPI(path='default/sections/programs-abc-'+DIGS+AZ_LOWER+'?limit=999&offset=0');
+	elements = jsonObj["items"]
 
 	for e in elements:
-		try: subtitle = e.xpath('./div/div/div[@class="title"]/text()')[0]
-		except: subtitle = ''
+		if e["available"]:
+			title = e["title"]
 
-		title = e.xpath('./a/div/h3//text()')[0].strip()
-		if len(subtitle.strip()) > 0 and title != subtitle:
-			title = title+': '+subtitle
+			try: thumb = Resource.ContentsOfURLWithFallback(e["images"]["nonretina_image"])
+			except: thumb = FallbackIcon
 
-		try: thumb = Resource.ContentsOfURLWithFallback(e.xpath('./a/div/img[@itemprop="thumbnailUrl"]//@data-src')[0])
-		except: thumb = FallbackIcon
-
-		url = e.xpath('./a[@itemprop="url"]//@href')[0]
-
-		if len(url.split('/')) == 3:
-			channel = url.split('/')[1]
-			series = url.split('/')[2]
-
-			url = Callback(ListRowsFromAJAX, path='series-'+series+'.'+channel+'_Episodes-serie/1/500', FallbackIcon=thumb, title2=title)
-
+			url = e["_links"]["self"]
 			oc.add(DirectoryObject(
 				title = title,
 				thumb = thumb,
 				key = url
 			))
-		else:
-			oc.add(VideoClipObject(
-				title = title,
-				thumb = thumb,
-				url = 'http://kijk.nl'+url
-			))
 
+	oc.objects.sort(key = lambda obj: obj.title.lower())
 	if len(oc) > 0:
 		return oc
 	else:
-		return ObjectContainer(header="Geen afleveringen beschikbaar", message="Er zijn geen afleveringen beschikbaar")
+		return ObjectContainer(header="Geen resultaten", message="Er zijn geen programma's gevonden")
+
+####################################################################################################
+def getFromAPI(path=''):
+	receivedJson = urllib.urlopen(API_URL_V1+path).read();
+	jsonObj = json.loads(receivedJson)
+	return jsonObj
+
+####################################################################################################
+def sortByTitle(t):
+	return t["title"]
